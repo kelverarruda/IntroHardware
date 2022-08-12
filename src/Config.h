75e3183c95601a2d4d4ec91b930694f3986d692b
kelverarruda <arrudakelver@gmail.com>
2022-08-11 21:23:32 -0300

    first commit

---
+++ src/Config.h
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#define DHTPIN D1
+
+DHT dht;
+WiFiClient nodeMCU;
+WiFiUDP ntpUDP;
+NTPClient ntpTime(ntpUDP, "a.ntp.br");
+
+PubSubClient client(nodeMCU);
+
+/*          WIFI SETTINGS           */
+const char* ssid = "<ssid>";
+const char* password = "<pass>";
+
+/*          MQTT SETTINGS            */
+const char* mqttServer = "mqtt.eclipseprojects.io";
+const int   mqttPort = 1883;
+const char* mqttClientId = "NodeMCU-ESP8266-RKLab";
+const char* mqttUser = "";
+const char* mqttPassword = "";
+const char* topicTemp = "rklab/temperature";
+const char* topicHumid = "rklab/humidity";
+const char* topicTempColor = "rklab/temperature-color";
+const char* topicTempHigh = "rklab/temperature-high";
+const char* topicTempLow = "rklab/temperature-low";
+
+/*          SETTINGS            */
+int tempHigh = 32;
+int tempLow = 18;
+const char* tempHighColor = "[217, 21, 17]";
+const char* tempLowhColor = "[37, 176, 248]";
+const char* tempNormalhColor = "[33, 217, 128]";
---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+
+#include "./Config.h"
+
+void verificaStatusWifi()
+{
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(500);
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(500);
+	}
+	digitalWrite(LED_BUILTIN, LOW);
+}
+
+void lerSensorDHT11()
+{
+	float temp = dht.getTemperature();
+	float humid = dht.getHumidity();
+	if (isnan(temp) || isnan(humid))
+	{
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+	if (temp >= tempHigh)
+	{
+		client.publish(topicTempColor, tempHighColor, true);
+		client.publish(topicTempHigh, String(temp).c_str(), true);
+	}
+	else if (temp <= tempLow)
+	{
+		client.publish(topicTempColor, tempLowhColor, true);
+		client.publish(topicTempLow, String(temp).c_str(), true);
+	}
+	else
+	{
+		client.publish(topicTempColor, tempNormalhColor, true);
+	}
+
+	client.publish(topicTemp, String(temp).c_str(), true);
+	client.publish(topicHumid, String(humid).c_str(), true);
+
+	ntpTime.update();
+	String formattedTime = ntpTime.getFormattedTime();
+	Serial.print(formattedTime + " -> ");
+	Serial.print("Temperature: ");
+	Serial.print(temp);
+	Serial.print(" | ");
+	Serial.print("Humidity: ");
+	Serial.println(humid);
+}
+
+void setup()
+{
+	Serial.begin(9600);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	dht.setup(DHTPIN);
+
+	WiFi.begin(ssid, password);
+	verificaStatusWifi();
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	client.setServer(mqttServer, mqttPort);
+
+	ntpTime.setTimeOffset(-3 * 3600);
+	ntpTime.begin();
+}
+
+void loop()
+{
+	verificaStatusWifi();
+	while (!client.connected())
+	{
+		client.connect(mqttClientId);
+		delay(500);
+		Serial.println("Initializing MQTT client...");
+	}
+	lerSensorDHT11();
+	delay(10000);
+}
